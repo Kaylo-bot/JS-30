4758c2329a3303fddc16b3dd4fb471916a6c8feb
Kaylobot <kaylobot@gmail.com>
2023-05-11 15:28:28 +0300

    day10

---
+++ Day-10 Check Multiple Checkboxes/script.js
@@ -0,0 +1,21 @@
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        checkBoxes.forEach(checkbox => {
+
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween;
+            }
+            if(inBetween){
+                checkbox.checked = true;
+            }
+        });
+    }
+    
+    lastChecked = this;
+}
+checkBoxes.forEach(checkBox => checkBox.addEventListener('click',handleCheck));
